95dd2614d25b49dea711e9049463c387b50511b8
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2023-04-21 20:30:32 +0700

    update respon & readme

--- models/photo.go
+++ models/photo.go
@@ -15,6 +15,46 @@ type Photo struct {
 	Comments []Comment `json:"comments,omitempty" gorm:"OnUpdate:CASCADE,OnDelete:SET NULL"`
 }
 
+type PhotoResponse struct {
+	Id       int               `json:"id"`
+	Title    string            `json:"title"`
+	Caption  string            `json:"caption"`
+	PhotoUrl string            `json:"photo_url"`
+	UserID   int               `json:"user_id,omitempty"`
+	User     *UserResponse     `json:"user,omitempty"`
+	Comments []CommentResponse `json:"comments,omitempty"`
+}
+
+func (u *Photo) ParseToModel() *PhotoResponse {
+	//get all comment
+	commentResp := []CommentResponse{}
+	for _, comment := range u.Comments {
+		commentResp = append(commentResp, *comment.ParseToModel())
+	}
+
+	if u.User != nil {
+		return &PhotoResponse{
+			Id:       u.Id,
+			Title:    u.Title,
+			Caption:  u.Caption,
+			PhotoUrl: u.PhotoUrl,
+			User:     u.User.ParseToModel(),
+			Comments: commentResp,
+		}
+	} else {
+		return &PhotoResponse{
+			Id:       u.Id,
+			Title:    u.Title,
+			Caption:  u.Caption,
+			PhotoUrl: u.PhotoUrl,
+			UserID:   u.UserID,
+			User:     nil,
+			Comments: commentResp,
+		}
+	}
+
+}
+
 // hooks
 func (u *Photo) BeforeCreate(tx *gorm.DB) (err error) {
 	_, err = govalidator.ValidateStruct(u)
